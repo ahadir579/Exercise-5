64a29a6f5d897191fb53e1feb9cdb9da9ff48e79
ahadir579 <139805536+ahadir579@users.noreply.github.com>
2023-07-27 00:19:31 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,13 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+// Allama Iqbal once said, “ Failure is not fatal until we surrender; trying again is the key to glorious victory.”
+
+//console.log(' Allama Iqbal once said, “ Failure is not fatal until we surrender; trying again is the key to glorious victory.”')
+
+let famous_person: string = "Allama Iqbal";
+let message: string = famous_person + ' once said, “ Failure is not fatal until we surrender; trying again is the key to glorious victory.”' 
+console.log(message)
